a7de807ef91c9634d9a0a5678488a54ba395d1ca
Chen Wainuo <83727873+chenwainuo@users.noreply.github.com>
2021-11-26 11:01:41 -0800

    add eth and btc for the ftx hedger

--- src/ftx_hedger.ts
+++ src/ftx_hedger.ts
@@ -1,8 +1,5 @@
 import MangoArbClient from "./mango";
 
-let prevMangoBalance = 0
-let prevFtxBalance = 0
-
 let localOrders = []
 const main = async () => {
 
@@ -10,7 +7,9 @@ const main = async () => {
     const mangoArbClient = new MangoArbClient('')
     await mangoArbClient.init(JSON.parse(''))
     const minChange = {
-        'SOL': 0.5
+        'SOL': 0.5,
+        'ETH': 0.03,
+        'BTC': 0.002
     }
 
     const findPrice = (orderbook, size) => {
@@ -29,76 +28,73 @@ const main = async () => {
 
             let mangoBalance = await mangoArbClient.getPositions()
 
-            const coins = (await mangoArbClient.ftx.getBalances())['result']
-            const ftxCoinBalance = coins.filter(p => p['coin'] === 'SOL')[0]['total']
+            const markets = ['ETH', 'BTC', 'SOL']
 
-            if (prevMangoBalance != mangoBalance || prevFtxBalance != ftxCoinBalance) {
-                console.log(new Date().toLocaleString().replace(',', ''), mangoBalance, ftxCoinBalance)
-                prevMangoBalance = mangoBalance
-                prevFtxBalance = ftxCoinBalance
-            }
+            for (const market of markets) {
+                const marketName = market + '/USD'
 
-            const delta = mangoBalance + ftxCoinBalance
-            const marketName = 'SOL' + '/USD'
+                const coins = (await mangoArbClient.ftx.getBalances())['result']
+                const ftxCoinBalance = coins.filter(p => p['coin'] === market)[0]['total']
 
-            if (Math.abs(delta) > minChange['SOL']) {
-                const orderbook = (await mangoArbClient.ftx.getOrderbook({marketName: marketName}))['result']
-                const side = delta < 0 ? 'buy' : 'sell'
-                let price = delta < 0 ? findPrice(orderbook['bids'], delta) : findPrice(orderbook['asks'], delta)
-                let orders = (await mangoArbClient.ftx.getOpenOrders(marketName))['result']
 
-                if (orders.length > 1) {
-                    await mangoArbClient.ftx.cancelAllOrders({market: marketName})
-                    console.log("canceled all orders")
-                    orders = []
-                }
-                if (orders.length === 1 && orders[0]['price'] != price) {
-                    if (orders[0]['side'] != side) {
-                        await mangoArbClient.ftx.cancelOrder(orders[0]['id'].toString())
-                        console.log("Wrong side, canceled top order")
-                    } else {
-                        const newOrder = await mangoArbClient.ftx.modifyOrder({
-                            orderId: orders[0]['id'].toString(),
-                            price: price,
-                            size: Math.abs(delta)
-                        })
-                        console.log("updated", orders[0]['id'].toString(), newOrder['result']['id'], newOrder['result']['side'], newOrder['result']['size'], newOrder['result']['price'])
+                const delta = mangoBalance[market] + ftxCoinBalance
+
+                if (Math.abs(delta) > minChange[market]) {
+                    const orderbook = (await mangoArbClient.ftx.getOrderbook({marketName: marketName}))['result']
+                    const side = delta < 0 ? 'buy' : 'sell'
+                    let price = delta < 0 ? findPrice(orderbook['bids'], delta) : findPrice(orderbook['asks'], delta)
+                    let orders = (await mangoArbClient.ftx.getOpenOrders(marketName))['result']
+
+                    if (orders.length > 1) {
+                        await mangoArbClient.ftx.cancelAllOrders({market: marketName})
+                        console.log(market, "canceled all orders")
+                        orders = []
                     }
-                }
-                if (orders.length === 0) {
-                    const currentTime = new Date().getTime() / 1000
-                    console.log("pre", localOrders)
-
-                    localOrders = localOrders.filter(p => {
-                        return p['market'] === marketName
-                            && p['side'] === side
-                            && p['epoch'] + 5 > currentTime
-                    })
-
-                    console.log("post", localOrders)
-
-                    if (localOrders.length) {
-                        console.log("already have order in local")
-                    } else {
-                        const order = await mangoArbClient.ftx.placeOrder(
-                            {
-                                market: marketName,
-                                side: side,
+                    if (orders.length === 1 && orders[0]['price'] != price) {
+                        if (orders[0]['side'] != side) {
+                            await mangoArbClient.ftx.cancelOrder(orders[0]['id'].toString())
+                            console.log(market,"Wrong side, canceled top order")
+                        } else {
+                            const newOrder = await mangoArbClient.ftx.modifyOrder({
+                                orderId: orders[0]['id'].toString(),
                                 price: price,
-                                size: Math.abs(delta),
-                                type: 'limit',
-                                postOnly: true
+                                size: Math.abs(delta)
                             })
-                        order['result']['epoch'] = currentTime
-                        console.log("placed", order['result']['id'], order['result']['side'], order['result']['size'], order['result']['price'])
-                        localOrders.push(order['result'])
+                            console.log(market, "updated", orders[0]['id'].toString(), newOrder['result']['id'], newOrder['result']['side'], newOrder['result']['size'], newOrder['result']['price'])
+                        }
+                    }
+                    if (orders.length === 0) {
+                        const currentTime = new Date().getTime() / 1000
+
+                        localOrders = localOrders.filter(p => {
+                            return p['market'] === marketName
+                                && p['side'] === side
+                                && p['epoch'] + 5 > currentTime
+                        })
+
+                        if (localOrders.length) {
+                            console.log(market, "already have order in local")
+                        } else {
+                            const order = await mangoArbClient.ftx.placeOrder(
+                                {
+                                    market: marketName,
+                                    side: side,
+                                    price: price,
+                                    size: Math.abs(delta),
+                                    type: 'limit',
+                                    postOnly: true
+                                })
+                            order['result']['epoch'] = currentTime
+                            console.log(market, "placed", order['result']['id'], order['result']['side'], order['result']['size'], order['result']['price'])
+                            localOrders.push(order['result'])
+                        }
                     }
                 }
             }
-            await new Promise(resolve => setTimeout(resolve, 1000));
         } catch (e) {
             console.log(e)
         } finally {
+            await new Promise(resolve => setTimeout(resolve, 1000));
             loop()
         }
     }
--- src/mango.ts
+++ src/mango.ts
@@ -14,15 +14,19 @@ import configFile from './ids.json';
 import {RestClient} from 'ftx-api'
 
 export default class MangoArbClient {
-    perpMarket: PerpMarket;
+    solPerpMarket: PerpMarket;
     connection: Connection;
     groupConfig: GroupConfig;
     client: MangoClient;
     mangoAccount: MangoAccount;
     mangoGroup: MangoGroup;
     owner: Account;
-    marketIndex: number
+    solMarketIndex: number
     ftx: RestClient;
+    btcPerpMarket: PerpMarket;
+    ethPerpMarket: PerpMarket;
+    btcMarketIndex: number;
+    ethMarketIndex: number;
 
     constructor(url: string) {
         const config = new Config(configFile);
@@ -42,20 +46,48 @@ export default class MangoArbClient {
     async init(privateKey) {
         this.client = new MangoClient(this.connection, this.groupConfig.mangoProgramId);
         // load group & market
-        const perpMarketConfig = getMarketByBaseSymbolAndKind(
+        const solPerpMarketConfig = getMarketByBaseSymbolAndKind(
             this.groupConfig,
             'SOL',
             'perp',
         );
-
-        this.marketIndex = perpMarketConfig.marketIndex;
-        this.mangoGroup = await this.client.getMangoGroup(this.groupConfig.publicKey);
-        this.perpMarket = await this.mangoGroup.loadPerpMarket(
-            this.connection,
-            perpMarketConfig.marketIndex,
-            perpMarketConfig.baseDecimals,
-            perpMarketConfig.quoteDecimals,
+        const btcPerpMarketConfig = getMarketByBaseSymbolAndKind(
+            this.groupConfig,
+            'BTC',
+            'perp',
         );
+        const ethPerpMarketConfig = getMarketByBaseSymbolAndKind(
+            this.groupConfig,
+            'ETH',
+            'perp',
+        );
+
+        this.solMarketIndex = solPerpMarketConfig.marketIndex;
+        this.btcMarketIndex = btcPerpMarketConfig.marketIndex;
+        this.ethMarketIndex = ethPerpMarketConfig.marketIndex;
+        this.mangoGroup = await this.client.getMangoGroup(this.groupConfig.publicKey);
+
+        this.solPerpMarket = await this.mangoGroup.loadPerpMarket(
+            this.connection,
+            solPerpMarketConfig.marketIndex,
+            solPerpMarketConfig.baseDecimals,
+            solPerpMarketConfig.quoteDecimals,
+        );
+
+        this.btcPerpMarket = await this.mangoGroup.loadPerpMarket(
+            this.connection,
+            btcPerpMarketConfig.marketIndex,
+            btcPerpMarketConfig.baseDecimals,
+            btcPerpMarketConfig.quoteDecimals,
+        )
+        this.ethPerpMarket = await this.mangoGroup.loadPerpMarket(
+            this.connection,
+            ethPerpMarketConfig.marketIndex,
+            ethPerpMarketConfig.baseDecimals,
+            ethPerpMarketConfig.quoteDecimals,
+        )
+
+
         this.owner = new Account(Uint8Array.from(privateKey));
         this.mangoAccount = (
             await this.client.getMangoAccountsForOwner(this.mangoGroup, this.owner.publicKey)
@@ -63,20 +95,6 @@ export default class MangoArbClient {
     }
 
     async refresh() {
-        const perpMarketConfig = getMarketByBaseSymbolAndKind(
-            this.groupConfig,
-            'SOL',
-            'perp',
-        );
-
-        this.marketIndex = perpMarketConfig.marketIndex;
-        this.mangoGroup = await this.client.getMangoGroup(this.groupConfig.publicKey);
-        this.perpMarket = await this.mangoGroup.loadPerpMarket(
-            this.connection,
-            perpMarketConfig.marketIndex,
-            perpMarketConfig.baseDecimals,
-            perpMarketConfig.quoteDecimals,
-        );
         this.mangoAccount = (
             await this.client.getMangoAccountsForOwner(this.mangoGroup, this.owner.publicKey)
         )[0];
@@ -84,22 +102,28 @@ export default class MangoArbClient {
 
 
     async getTopBid() {
-        let bids = await this.perpMarket.loadBids(this.connection);
+        let bids = await this.solPerpMarket.loadBids(this.connection);
         return bids.getL2(1)[0][0]
     }
 
     async getTopAsk() {
-        let asks = await this.perpMarket.loadAsks(this.connection);
+        let asks = await this.solPerpMarket.loadAsks(this.connection);
         return asks.getL2(1)[0][0]
     }
 
     async getPositions() {
         await this.refresh()
-        return this.mangoAccount.getPerpPositionUi(this.marketIndex, this.perpMarket)
+
+        const SOL = this.mangoAccount.getPerpPositionUi(this.solMarketIndex, this.solPerpMarket)
+        const ETH = this.mangoAccount.getPerpPositionUi(this.ethMarketIndex, this.ethPerpMarket)
+        const BTC = this.mangoAccount.getPerpPositionUi(this.btcMarketIndex, this.btcPerpMarket)
+        return {
+            SOL, ETH, BTC
+        }
     }
 
     marketLong(usdAmount, topAsk, quantity) {
-        const [nativePrice, nativeQuantity] = this.perpMarket.uiToNativePriceQuantity(
+        const [nativePrice, nativeQuantity] = this.solPerpMarket.uiToNativePriceQuantity(
             topAsk,
             quantity,
         );
@@ -110,10 +134,10 @@ export default class MangoArbClient {
             this.mangoAccount.publicKey,
             this.owner.publicKey,
             this.mangoGroup.mangoCache,
-            this.perpMarket.publicKey,
-            this.perpMarket.bids,
-            this.perpMarket.asks,
-            this.perpMarket.eventQueue,
+            this.solPerpMarket.publicKey,
+            this.solPerpMarket.bids,
+            this.solPerpMarket.asks,
+            this.solPerpMarket.eventQueue,
             this.mangoAccount.spotOpenOrders,
             nativePrice,
             nativeQuantity,
@@ -124,7 +148,7 @@ export default class MangoArbClient {
     }
 
     marketShort(usdAmount, topBid, quantity) {
-        const [nativePrice, nativeQuantity] = this.perpMarket.uiToNativePriceQuantity(
+        const [nativePrice, nativeQuantity] = this.solPerpMarket.uiToNativePriceQuantity(
             topBid,
             quantity,
         );
@@ -135,10 +159,10 @@ export default class MangoArbClient {
             this.mangoAccount.publicKey,
             this.owner.publicKey,
             this.mangoGroup.mangoCache,
-            this.perpMarket.publicKey,
-            this.perpMarket.bids,
-            this.perpMarket.asks,
-            this.perpMarket.eventQueue,
+            this.solPerpMarket.publicKey,
+            this.solPerpMarket.bids,
+            this.solPerpMarket.asks,
+            this.solPerpMarket.eventQueue,
             this.mangoAccount.spotOpenOrders,
             nativePrice,
             nativeQuantity,
@@ -148,3 +172,4 @@ export default class MangoArbClient {
         )
     }
 }
+
