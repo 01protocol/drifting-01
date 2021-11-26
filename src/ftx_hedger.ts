49951e1974922a21bd2e80e9d3f54c225fd48c1b
Chen Wainuo <83727873+chenwainuo@users.noreply.github.com>
2021-11-25 16:32:03 -0800

    wrap loop() in fianlly

--- src/ftx_hedger.ts
+++ src/ftx_hedger.ts
@@ -25,77 +25,82 @@ const main = async () => {
     }
 
     async function loop() {
+        try {
 
-        let mangoBalance = await mangoArbClient.getPositions()
+            let mangoBalance = await mangoArbClient.getPositions()
 
-        const coins = (await mangoArbClient.ftx.getBalances())['result']
-        const ftxCoinBalance = coins.filter(p => p['coin'] === 'SOL')[0]['total']
+            const coins = (await mangoArbClient.ftx.getBalances())['result']
+            const ftxCoinBalance = coins.filter(p => p['coin'] === 'SOL')[0]['total']
 
-        if (prevMangoBalance != mangoBalance || prevFtxBalance != ftxCoinBalance) {
-            console.log(new Date().toLocaleString().replace(',', ''), mangoBalance, ftxCoinBalance)
-            prevMangoBalance = mangoBalance
-            prevFtxBalance = ftxCoinBalance
-        }
-
-        const delta = mangoBalance + ftxCoinBalance
-        const marketName = 'SOL' + '/USD'
-
-        if (Math.abs(delta) > minChange['SOL']) {
-            const orderbook = (await mangoArbClient.ftx.getOrderbook({marketName: marketName}))['result']
-            const side = delta < 0 ? 'buy' : 'sell'
-            let price = delta < 0 ? findPrice(orderbook['bids'], delta) : findPrice(orderbook['asks'], delta)
-            let orders = (await mangoArbClient.ftx.getOpenOrders(marketName))['result']
-
-            if (orders.length > 1) {
-                await mangoArbClient.ftx.cancelAllOrders({market: marketName})
-                console.log("canceled all orders")
-                orders = []
+            if (prevMangoBalance != mangoBalance || prevFtxBalance != ftxCoinBalance) {
+                console.log(new Date().toLocaleString().replace(',', ''), mangoBalance, ftxCoinBalance)
+                prevMangoBalance = mangoBalance
+                prevFtxBalance = ftxCoinBalance
             }
-            if (orders.length === 1 && orders[0]['price'] != price) {
-                if (orders[0]['side'] != side) {
-                    await mangoArbClient.ftx.cancelOrder(orders[0]['id'].toString())
-                    console.log("Wrong side, canceled top order")
-                } else {
-                    const newOrder = await mangoArbClient.ftx.modifyOrder({
-                        orderId: orders[0]['id'].toString(),
-                        price: price,
-                        size: Math.abs(delta)
-                    })
-                    console.log("updated", orders[0]['id'].toString(), newOrder['result']['id'], newOrder['result']['side'], newOrder['result']['size'], newOrder['result']['price'])
+
+            const delta = mangoBalance + ftxCoinBalance
+            const marketName = 'SOL' + '/USD'
+
+            if (Math.abs(delta) > minChange['SOL']) {
+                const orderbook = (await mangoArbClient.ftx.getOrderbook({marketName: marketName}))['result']
+                const side = delta < 0 ? 'buy' : 'sell'
+                let price = delta < 0 ? findPrice(orderbook['bids'], delta) : findPrice(orderbook['asks'], delta)
+                let orders = (await mangoArbClient.ftx.getOpenOrders(marketName))['result']
+
+                if (orders.length > 1) {
+                    await mangoArbClient.ftx.cancelAllOrders({market: marketName})
+                    console.log("canceled all orders")
+                    orders = []
                 }
-            }
-            if (orders.length === 0) {
-                const currentTime = new Date().getTime() / 1000
-                console.log("pre", localOrders)
-
-                localOrders = localOrders.filter(p => {
-                    return p['market'] === marketName
-                        && p['side'] === side
-                        && p['epoch'] + 5 > currentTime
-                })
-
-                console.log("post", localOrders)
-
-                if (localOrders.length) {
-                    console.log("already have order in local")
-                } else {
-                    const order = await mangoArbClient.ftx.placeOrder(
-                        {
-                            market: marketName,
-                            side: side,
+                if (orders.length === 1 && orders[0]['price'] != price) {
+                    if (orders[0]['side'] != side) {
+                        await mangoArbClient.ftx.cancelOrder(orders[0]['id'].toString())
+                        console.log("Wrong side, canceled top order")
+                    } else {
+                        const newOrder = await mangoArbClient.ftx.modifyOrder({
+                            orderId: orders[0]['id'].toString(),
                             price: price,
-                            size: Math.abs(delta),
-                            type: 'limit',
-                            postOnly: true
+                            size: Math.abs(delta)
                         })
-                    order['result']['epoch'] = currentTime
-                    console.log("placed", order['result']['id'], order['result']['side'], order['result']['size'], order['result']['price'])
-                    localOrders.push(order['result'])
+                        console.log("updated", orders[0]['id'].toString(), newOrder['result']['id'], newOrder['result']['side'], newOrder['result']['size'], newOrder['result']['price'])
+                    }
+                }
+                if (orders.length === 0) {
+                    const currentTime = new Date().getTime() / 1000
+                    console.log("pre", localOrders)
+
+                    localOrders = localOrders.filter(p => {
+                        return p['market'] === marketName
+                            && p['side'] === side
+                            && p['epoch'] + 5 > currentTime
+                    })
+
+                    console.log("post", localOrders)
+
+                    if (localOrders.length) {
+                        console.log("already have order in local")
+                    } else {
+                        const order = await mangoArbClient.ftx.placeOrder(
+                            {
+                                market: marketName,
+                                side: side,
+                                price: price,
+                                size: Math.abs(delta),
+                                type: 'limit',
+                                postOnly: true
+                            })
+                        order['result']['epoch'] = currentTime
+                        console.log("placed", order['result']['id'], order['result']['side'], order['result']['size'], order['result']['price'])
+                        localOrders.push(order['result'])
+                    }
                 }
             }
+            await new Promise(resolve => setTimeout(resolve, 1000));
+        } catch (e) {
+            console.log(e)
+        } finally {
+            loop()
         }
-        await new Promise(resolve => setTimeout(resolve, 1000));
-        loop()
     }
 
     await loop()
